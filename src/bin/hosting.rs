cc45b42e7165613650e375f6488b7aec7393372c
Jaemin Kim <geminik23@gmail.com>
2021-06-11 08:51:53 +0900

    log

--- src/bin/hosting.rs
+++ src/bin/hosting.rs
@@ -13,7 +13,6 @@ async fn main() -> Result<(), std::io::Error> {
     let _ = dotenv::dotenv().ok();
     env_logger::init();
 
-    tide::log::start();
     let mut app = tide::new();
 
     let path = std::env::var("IMAGEHOSTING_PATH").expect("no env var IMAGEHOSTING_PATH");
